0cf1057ebfd38a0ffa238e39eb959a93cd517fec
Markus Vickman~ <markus.vickman@hotmail.com>
2024-04-16 15:53:51 +0200

    get och post fungerar med mongoDB, ska fortsätta med put och delete

--- server.js
+++ server.js
@@ -1,29 +1,6 @@
-//express server med api hostad hos azure som lagrar och hämtar data från en heliohost mysql databas.
+//express server med api hostad hos azure som lagrar och hämtar data från en mongoDB databas.
 //Variable .env
 require('dotenv').config({ path: './.env' });
-//Lägger till mysql och ansluter
-const mysql = require("mysql");
-
-//anslutning till mySQL databas
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.HOST,
-    user: process.env.USER_ACC,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
-
-//Ger meddelande vid anslutning eller vid misslyckad.
-/*connection.connect((err) => {
-    if (err) {
-        console.error("Connection failed big!: " + err);
-       // throw err; Ger fel av Host-servern
-    }
-
-    console.log("Connected to MySQL!");
-});*/
-
-
 //lägger till express och cors för att kunna ansluta från vilken adress som helst
 const express = require('express');
 const cors = require('cors');
@@ -35,45 +12,90 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+
+//Lägger till mongoose
+const mongoose = require('mongoose');
+const { type } = require('express/lib/response');
+
+//Ansluter till mongoDB
+mongoose.connect("mongodb://localhost:27017/developer_dt207g").then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Error connecting to database: " + error);
+})
+
+// Skapa ett db-schema
+const WorkSchema = mongoose.Schema({
+    indexId: {
+        type: String,
+        required: false
+    },
+    companyName: {
+        type: String,
+        required: true
+    },
+    jobTitle: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+
+// Skapa en model
+const WorkExperience = mongoose.model('Work_experience', WorkSchema);
+
+
 //Välkomst meddelande om webbadress/api anropas
 app.get('/api', (req, res) => {
     res.json({ message: 'Welcome to my CV api!' });
 });
 
 //hämtar data från mySQL server och skickar med det som svart i fetch förfrågan om webbadress/api/cv anropas. Skickar felmeddelande om fel uppstår hos databasen.
-app.get('/api/cv', (req, res) => {
-    pool.getConnection(function (err, connection) {
-        if (err) throw err; // not connected!
+app.get('/api/cv', async (req, res) => {
+    try {
 
-        connection.query("SELECT * FROM WORK_EXPERIENCE;", (err, rows) => {
-            if (err) {
-                res.status(500).json({ error: "Could not reach database. " + err });
-                throw err;
-            }
-            else {
-                res.json(rows);
-                // släpper databasanslutningen.
-                connection.release();
-                // hanterar fel vid släpp.
-                if (err) throw err;
-            }
-        });
-    });
+        //connection.query("SELECT * FROM WORK_EXPERIENCE;", (err, rows) => {
+
+        let result = await WorkExperience.find();
+        return res.json(result);
+
+    } catch (error) {
+        return res.status(500).json({ error: "Could not reach database. " + error });
+    }
 });
 
 //lägger till data till mySQL servern från post-anropet om webbadress/api/add anropas. Skickar felmeddelande om fel uppstår hos databasen.
-app.post('/api/add', (req, res) => {
-    let companyName = req.body.companyName;
-    let jobTitle = req.body.jobTitle;
-    let location = req.body.location;
-    let startDate = req.body.startDate;
-    let endDate = req.body.endDate;
-    let description = req.body.description;
+app.post('/api/add', async (req, res) => {
+
+    let workExperience1 = {
+        companyName: req.body.companyName,
+        jobTitle: req.body.jobTitle,
+        location: req.body.location,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        description: req.body.description
+    };
 
     let error = {};
 
     //Felhantering om uppgifter saknas
-    if (!companyName || !jobTitle || !location || !startDate || !endDate || !description) {
+    if (!workExperience1.companyName || !workExperience1.jobTitle || !workExperience1.location || !workExperience1.startDate || !workExperience1.endDate || !workExperience1.description) {
         error = {
             message: "Parameters missing in the request.",
             detail: "Post request most include companyName, jobTitle, location, startDate, endDate and description",
@@ -86,35 +108,28 @@ app.post('/api/add', (req, res) => {
     }
     //Om allt är korrekt körs frågan till mySQL-databasen för att lagre det nya cv
     else {
-        pool.getConnection(function (err, connection) {
-            if (err) throw err; // not connected!
-
-            connection.query("INSERT INTO WORK_EXPERIENCE(COMPANY_NAME, JOB_TITLE, LOCATION, START_DATE, END_DATE, DESCRIPTION) VALUES(?,?,?,?,?,?)", [companyName, jobTitle, location, startDate, endDate, description], (err, result) => {
-                if (err) {
-                    res.status(500).json({ error: "Database error. " + err });
-                    throw err;
-                }
-                else {
-                    res.json({ Success: "Post data stored in database." });
-                    // släpper databasanslutningen.
-                    connection.release();
-                    // hanterar fel vid släpp.
-                    if (err) throw err;
-                }
-            });
-        });
+        try {
+            await WorkExperience.create(workExperience1);
+            return res.json({ Success: "Post data stored in database." });
+        } catch (error) {
+            return res.status(500).json({ error: "Database error. " + error });
+        }
     }
 });
 
+
 //Ändrar rader i mySQL-databasen när förfrågan till webbadress/api/edit görs. Skickar felmeddelande om fel uppstår hos databasen.
-app.put('/api/edit', (req, res) => {
-    let indexId = req.body.indexId;
-    let companyName = req.body.companyName;
-    let jobTitle = req.body.jobTitle;
-    let location = req.body.location;
-    let startDate = req.body.startDate;
-    let endDate = req.body.endDate;
-    let description = req.body.description;
+app.put('/api/edit', async(req, res) => {
+
+    let workExperience1 = {
+        indexId: req.body.indexId,
+        companyName: req.body.companyName,
+        jobTitle: req.body.jobTitle,
+        location: req.body.location,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        description: req.body.description
+    };
 
     let error = {
         message: "Parameters missing in the request.",
@@ -134,29 +149,17 @@ app.put('/api/edit', (req, res) => {
     }
     //värdet skrivs in på rätt index i rätt kolomn i databasen.
     else {
-        pool.getConnection(function (err, connection) {
-            if (err) throw err; // not connected!
-
-            connection.query("UPDATE WORK_EXPERIENCE SET COMPANY_NAME=?, JOB_TITLE=?, LOCATION=?, START_DATE=?, END_DATE=?, DESCRIPTION=? WHERE ID=?", [companyName, jobTitle, location, startDate, endDate, description, indexId], (err) => {
-                if (err) {
-                    res.status(500).json({ error: "Database error. " + err });
-                    throw err;
-                }
-                else {
-                    res.status(200).json({ Success: "Put data updated in database." });
-                    // släpper databasanslutningen.
-                    connection.release();
-                    // hanterar fel vid släpp.
-                    if (err) throw err;
-                }
-            });
-        });
+        try {
+            await WorkExperience.create(workExperience1);
+            return res.status(200).json({ Success: "Put data updated in database." });
+        } catch (error) {
+            return res.status(500).json({ error: "Database error. " + error });
+        }
     }
-
 });
 
 //tar bort data från mySQL server när förfrågan till webbadress/api/cv görs. Skickar felmeddelande om fel uppstår hos databasen.
-app.delete('/api/delete/:id', (req, res) => {
+app.delete('/api/delete/:id', async(req, res) => {
     let id = req.params.id;
 
     pool.getConnection(function (err, connection) {
